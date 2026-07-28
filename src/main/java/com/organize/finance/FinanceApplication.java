7208fe938b42b9182ba7c14126c0f505ed48be06
Andre <andreluizselestino@gmail.com>
2026-07-27 22:57:50 -0300

    feature: tirando spring security

--- src/main/java/com/organize/finance/FinanceApplication.java
+++ src/main/java/com/organize/finance/FinanceApplication.java
@@ -1,9 +1,12 @@
 package com.organize.finance;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 public class FinanceApplication {
 
     public static void main(String[] args) {
