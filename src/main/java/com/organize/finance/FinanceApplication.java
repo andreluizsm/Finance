7fc46814b1f65dd075ddaf2d7ee7de3623ae421f
Andre <andreluizselestino@gmail.com>
2026-07-29 23:33:35 -0300

    feature: add propriedades do config cors

--- src/main/java/com/organize/finance/FinanceApplication.java
+++ src/main/java/com/organize/finance/FinanceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication
 public class FinanceApplication {
 
     public static void main(String[] args) {
