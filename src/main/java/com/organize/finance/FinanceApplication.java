ad8c129812397d63d866392b5060655779645f40
Andre <andreluizselestino@gmail.com>
2026-08-03 19:26:21 -0300

    feature: alteracoes para o mapStruct funcionar

--- src/main/java/com/organize/finance/FinanceApplication.java
+++ src/main/java/com/organize/finance/FinanceApplication.java
@@ -1,10 +1,7 @@
 package com.organize.finance;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
-import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
 @SpringBootApplication
 public class FinanceApplication {
