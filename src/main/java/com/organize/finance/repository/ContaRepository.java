dd26900a82c78cb93982ec28f81bf55967693154
Andre <andreluizselestino@gmail.com>
2026-08-07 20:52:36 -0300

    feature: criacao da Conta

--- src/main/java/com/organize/finance/repository/ContaRepository.java
+++ src/main/java/com/organize/finance/repository/ContaRepository.java
@@ -1,8 +1,9 @@
 package com.organize.finance.repository;
 
+import com.organize.finance.bases.baseRepository.baseRepository;
 import com.organize.finance.domain.Conta;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ContaRepository {
+public interface ContaRepository extends baseRepository<Conta> {
 }
---
+++ src/main/java/com/organize/finance/service/ContaService.java
@@ -0,0 +1,35 @@
+package com.organize.finance.service;
+
+import com.organize.finance.bases.baseService.baseService;
+import com.organize.finance.domain.dto.ContaDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ContaService extends baseService<ContaDto> {
+    @Override
+    public ContaDto findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<ContaDto> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public ContaDto create(ContaDto entity) {
+        return null;
+    }
+
+    @Override
+    public ContaDto update(ContaDto entity) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
