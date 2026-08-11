fc8b817fda72a205e755a238d6f26c4bc56e5af3
Andre <andreluizselestino@gmail.com>
2026-08-10 21:16:08 -0300

    feature: adicionando bean do mapper

--- src/main/java/com/organize/finance/mapper/TransacaoMapper.java
+++ src/main/java/com/organize/finance/mapper/TransacaoMapper.java
@@ -3,9 +3,11 @@ package com.organize.finance.mapper;
 import com.organize.finance.bases.baseMapper.baseMapper;
 import com.organize.finance.domain.Transacao;
 import com.organize.finance.domain.dto.TransacaoDto;
+import org.mapstruct.Mapper;
 
 import java.util.List;
 
+@Mapper(componentModel = "spring")
 public interface TransacaoMapper extends baseMapper<Transacao, TransacaoDto> {
     @Override
     TransacaoDto objToDto(Transacao obj);
