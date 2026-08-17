d3da4d5bd8e77f974f77852c3ceeaba136ae58da
Andre <andreluizselestino@gmail.com>
2026-08-17 19:57:55 -0300

    feature: configs jwt

--- src/main/java/com/organize/finance/repository/UserRepository.java
+++ src/main/java/com/organize/finance/repository/UserRepository.java
@@ -4,6 +4,10 @@ import com.organize.finance.domain.User;
 import org.springframework.stereotype.Repository;
 import com.organize.finance.bases.baseRepository.baseRepository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends baseRepository<User> {
+
+    Optional<User> findByEmail(String email);
 }
